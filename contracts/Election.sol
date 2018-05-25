329234062d1fc0c8112e53e5d6bac00efa54e7e0
Rafel Villarreal <rvillarreal416@gmail.com>
2018-05-24 21:14:59 -0500

    package json

--- contracts/Election.sol
+++ contracts/Election.sol
@@ -1,10 +1,20 @@
-pragma solidity ^0.4.11;
+pragma solidity ^0.4.2;
 
 contract Election {
 
-  string public candidate;
+  //Model a candidate
+  struct Candidate {
+    uint uid;
+    string name;
+    uint voteCount;
+  }
+
+  // Store a candidate
+  //Fetch candidate
+  mapping (uint => Candidate) public candidates;
+  //Store candidates count
 
   constructor() {
-    candidate = 'Candidate 1';
+
   }
 }
