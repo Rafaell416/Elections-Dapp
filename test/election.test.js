35ddd8c5782ecd9c2f7f71f316e543df3c80a433
Rafel Villarreal <rvillarreal416@gmail.com>
2018-05-26 15:39:39 -0500

    2nd test implemented to verify correct values

--- test/election.test.js
+++ test/election.test.js
@@ -1,9 +1,30 @@
 let Election = artifacts.require('Election.sol')
 
 contract('Election', () => {
+  let electionInstance
+
   it('initializes with 2 candidates', () => {
     return Election.deployed()
       .then(instance => instance.candidatesCount())
       .then(count => assert.equal(count, 2))
+  })
+
+  it('initializes the contract with correct values', () => {
+    return Election.deployed()
+      .then(instance => {
+        electionInstance = instance
+        return electionInstance.candidates(1)
+      })
+      .then(candidate => {
+        assert.equal(candidate[0], 1, 'Contains the correct id')
+        assert.equal(candidate[1], 'Candidate 1', 'Contains the correct name')
+        assert.equal(candidate[2], 0, 'Contains the correct amount of votes')
+        return electionInstance.candidates(2)
+      })
+      .then(candidate => {
+        assert.equal(candidate[0], 2, 'Contains the correct id')
+        assert.equal(candidate[1], 'Candidate 2', 'Contains the correct name')
+        assert.equal(candidate[2], 0, 'Contains the correct amount of votes')
+      })
   })
 })
