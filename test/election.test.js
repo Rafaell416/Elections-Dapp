6766a3691c17002930ade1f8868f1173081f75ee
Rafel Villarreal <rvillarreal416@gmail.com>
2018-05-26 23:17:22 -0500

    test for double voting

--- test/election.test.js
+++ test/election.test.js
@@ -32,8 +32,8 @@ contract('Election', (accounts) => {
   it('allows a voter to cast a vote', () => {
     return Election.deployed()
     .then(instance => {
-      electionInstance = instance;
-      candidateId = 1;
+      electionInstance = instance
+      candidateId = 1
       return electionInstance.vote(candidateId, { from: accounts[0] })
     })
     .then(receipt => electionInstance.voters(accounts[0]))
@@ -44,6 +44,56 @@ contract('Election', (accounts) => {
     .then(candidate => {
       let voteCount = candidate[2]
       assert.equal(voteCount, 1, 'increments the candidates vote count')
+    })
+  })
+
+  it("throws an exception for invalid candiates", () => {
+    return Election.deployed()
+    .then((instance) => {
+      electionInstance = instance
+      return electionInstance.vote(99, { from: accounts[1] })
+    })
+    .then(assert.fail).catch((error) => {
+      assert(error.message.indexOf('revert') >= 0, "error message must contain revert")
+      return electionInstance.candidates(1)
+    })
+    .then((candidate1) => {
+      let voteCount = candidate1[2]
+      assert.equal(voteCount, 1, "candidate 1 did not receive any votes")
+      return electionInstance.candidates(2)
+    })
+    .then((candidate2) => {
+      let voteCount = candidate2[2]
+      assert.equal(voteCount, 0, "candidate 2 did not receive any votes")
+    })
+  })
+
+  it("throws an exception for double voting", () => {
+    return Election.deployed()
+    .then((instance) => {
+      electionInstance = instance
+      candidateId = 2
+      electionInstance.vote(candidateId, { from: accounts[1] })
+      return electionInstance.candidates(candidateId)
+    })
+    .then((candidate) => {
+      let voteCount = candidate[2]
+      assert.equal(voteCount, 1, "accepts first vote")
+      // Try to vote again
+      return electionInstance.vote(candidateId, { from: accounts[1] })
+    })
+    .then(assert.fail).catch((error) => {
+      assert(error.message.indexOf('revert') >= 0, "error message must contain revert")
+      return electionInstance.candidates(1)
+    })
+    .then((candidate1) => {
+      let voteCount = candidate1[2]
+      assert.equal(voteCount, 1, "candidate 1 did not receive any votes")
+      return electionInstance.candidates(2)
+    })
+    .then((candidate2) => {
+      let voteCount = candidate2[2]
+      assert.equal(voteCount, 1, "candidate 2 did not receive any votes")
     })
   })
 })
